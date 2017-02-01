12292cbebf4b9ef48e348a0fa18e589df2d76f31
Dmitry Tymchuk <dmitry@Dmitrys-MBP.local>
2017-02-01 14:52:16 +0100

    More tests for models

--- test/models/experiment_test.exs
+++ test/models/experiment_test.exs
@@ -1,24 +1,51 @@
 defmodule Xperiments.ExperimentTest do
   use Xperiments.ModelCase
 
-  alias Xperiments.Experiment
+  alias Xperiments.{Experiment, User, Application}
 
-  @valid_attrs %{description: "some content", end_date: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}, max_users: 42, name: "some content", sampling_rate: "120.5", start_date: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}}
+  @valid_attrs %{description: "some content", end_date: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}, max_users: 42, name: "some content", start_date: %{day: 17, hour: 14, min: 0, month: 4, sec: 0, year: 2010}}
   @invalid_attrs %{}
+  @experiment %Experiment{user: %User{}, application: %Application{}}
 
   test "changeset with valid attributes" do
-    changeset = Experiment.changeset(%Experiment{}, @valid_attrs)
+    changeset = Experiment.changeset(@experiment, @valid_attrs)
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
-    changeset = Experiment.changeset(%Experiment{}, @invalid_attrs)
+    changeset = Experiment.changeset(@experiment, @invalid_attrs)
     refute changeset.valid?
   end
 
-  test "references with parent models" do
-    changeset = Experiment.changeset(%Experiment{}, Map.drop(@valid_attrs, [:sampling_rate, :max_users]))
+  test "changeset without an association" do
+    changeset = Experiment.changeset(Map.delete(@experiment, :user), @valid_attrs)
     refute changeset.valid?
-    assert changeset.errors == [sampling_rate: {"required at least one field", []}]
+  end
+
+  test "validation when at least one required field is not set" do
+    changeset = Experiment.changeset(@experiment, Map.drop(@valid_attrs, [:sampling_rate, :max_users]))
+    refute changeset.valid?
+    assert changeset.errors == [__shared__: {"required at least one field", []}]
+  end
+
+  test "number validation when a field can be set or not" do
+    changeset = Experiment.changeset(@experiment, %{@valid_attrs | max_users: 0})
+    refute changeset.valid?
+    assert changeset.errors == [max_users: {"must be greater than %{number}", [validation: :number, number: 0]}]
+  end
+
+  test "validate embedded fields: variants and rules" do
+    variants = [
+      %{name: "var a", allocation: 1, description: "nothing", payload: "test"}
+    ]
+    rules = [
+      %{parameter: "lang", type: "string", operator: "==", value: "ru"}
+    ]
+    changeset = Experiment.changeset_with_embeds(@experiment,
+      Map.merge(@valid_attrs, %{variants: variants, rules: rules}))
+    assert changeset.valid?
+
+    changeset = Experiment.changeset_with_embeds(@experiment, @valid_attrs)
+    refute changeset.valid?
   end
 end
--- web/models/rule.ex
+++ web/models/rule.ex
@@ -1,9 +1,30 @@
 defmodule Xperiments.Rule do
+  @moduledoc """
+  Defines rules for a segmentation.
+
+  A rule set in form:
+  - *parameter*. String. Example: "region", "country", "lang", etc.
+  - *type*. String. Can be only: "string", "number", "boolean"
+  - *operator*. String. Can be only: "==", "!=", ">", "<", ">=", "<="
+  - *value*. String. Example: "eu", "ru", "100", etc.
+  """
   use Xperiments.Web, :model
 
   embedded_schema do
     field :parameter
-    field :operator
+    field :type, :string, default: "string"
+    field :operator, :string, default: "=="
     field :value
   end
+
+  @allowed_params ~w(parameter type operator value)a
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @allowed_params)
+    |> validate_required(@allowed_params)
+    |> validate_inclusion(:type, ~w(string number))
+    |> validate_inclusion(:operator, ~w(== != > < >= <=))
+  end
+
 end
