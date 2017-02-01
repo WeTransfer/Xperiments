a650d5cb30aad9a2c391576fa19e3d33b6cf4560
Dmitry Tymchuk <dmitry@Dmitrys-MBP.local>
2017-02-01 18:45:23 +0100

    Validate dates

--- test/models/experiment_test.exs
+++ test/models/experiment_test.exs
@@ -5,49 +5,74 @@ defmodule Xperiments.ExperimentTest do
   import Xperiments.Factory
   alias Xperiments.{Experiment, User, Application}
 
-  @valid_attrs %{description: "some content", end_date: Timex.shift(Timex.now(), days: 3) |> Timex.format!("{ISO:Extended:Z}"), max_users: 42, name: "some content", start_date: Timex.now()}
-  @invalid_attrs %{}
-  @experiment %Experiment{user: %User{}, application: %Application{}}
+  def build_date(shift_days) do
+    Timex.now()
+    |> Timex.shift(days: shift_days)
+    |> Timex.format!("{ISO:Extended:Z}")
+  end
 
-  test "changeset with valid attributes" do
-    changeset = Experiment.changeset(@experiment, @valid_attrs)
+  setup context do
+    [
+      valid_attrs: %{description: "some content",
+                     end_date: build_date(3),
+                     max_users: 42,
+                     name: "some content",
+                     start_date: build_date(1)},
+      experiment:  %Experiment{user: %User{}, application: %Application{}}
+    ]
+  end
+
+  test "changeset with valid attributes", context do
+    changeset = Experiment.changeset(context[:experiment], context[:valid_attrs])
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Experiment.changeset(@experiment, @invalid_attrs)
+  test "changeset with invalid attributes", context do
+    changeset = Experiment.changeset(context[:experiment], %{})
     refute changeset.valid?
   end
 
-  test "changeset without an association" do
-    changeset = Experiment.changeset(Map.delete(@experiment, :user), @valid_attrs)
+  test "changeset without an association", context do
+    changeset = Experiment.changeset(Map.delete(context[:experiment], :user), context[:valid_attrs])
     refute changeset.valid?
   end
 
-  test "validation when at least one required field is not set" do
-    changeset = Experiment.changeset(@experiment, Map.drop(@valid_attrs, [:sampling_rate, :max_users]))
+  test "validation when at least one required field is not set", context do
+    changeset = Experiment.changeset(context[:experiment], Map.drop(context[:valid_attrs], [:sampling_rate, :max_users]))
     refute changeset.valid?
     assert changeset.errors == [__shared__: {"required at least one field", []}]
   end
 
-  test "number validation when a field can be set or not" do
-    changeset = Experiment.changeset(@experiment, %{@valid_attrs | max_users: 0})
+  test "number validation when a field can be set or not", context do
+    changeset = Experiment.changeset(context[:experiment], %{context[:valid_attrs] | max_users: 0})
     refute changeset.valid?
     assert changeset.errors == [max_users: {"must be greater than %{number}", [validation: :number, number: 0]}]
   end
 
-  test "validate embedded fields: variants and rules" do
+  test "validate embedded fields: variants and rules", context do
     variants = [
       %{name: "var a", allocation: 1, description: "nothing", payload: "test"}
     ]
     rules = [
       %{parameter: "lang", type: "string", operator: "==", value: "ru"}
     ]
-    changeset = Experiment.changeset_with_embeds(@experiment,
-      Map.merge(@valid_attrs, %{variants: variants, rules: rules}))
+    changeset = Experiment.changeset_with_embeds(context[:experiment],
+      Map.merge(context[:valid_attrs], %{variants: variants, rules: rules}))
     assert changeset.valid?
 
-    changeset = Experiment.changeset_with_embeds(@experiment, @valid_attrs)
+    changeset = Experiment.changeset_with_embeds(context[:experiment], context[:valid_attrs])
+    refute changeset.valid?
+  end
+
+  test "changeset with a start date greater than an end date", context do
+    attrs = %{context[:valid_attrs] | end_date: build_date(-1)}
+    changeset = Experiment.changeset(context[:experiment], attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset with a date in the past", context do
+    attrs = %{context[:valid_attrs] | start_date: build_date(-1)}
+    changeset = Experiment.changeset(context[:experiment], attrs)
     refute changeset.valid?
   end
 
--- web/models/experiment.ex
+++ web/models/experiment.ex
@@ -65,6 +65,9 @@ defmodule Xperiments.Experiment do
     |> cast_assoc(:user, required: true)
     |> cast_assoc(:application, required: true)
     |> validate_required([:name, :description, :start_date, :end_date])
+    |> validate_end_date_greater_start_date([:start_date, :end_date])
+    |> validate_current_or_future_date(:start_date)
+    |> validate_current_or_future_date(:end_date)
     |> validate_required_at_least_one([:sampling_rate, :max_users])
     |> maybe_validate_number(:sampling_rate, greater_than: 0)
     |> maybe_validate_number(:max_users, greater_than: 0)
@@ -75,6 +78,29 @@ defmodule Xperiments.Experiment do
     |> changeset(params)
     |> cast_embed(:variants, required: true)
     |> cast_embed(:rules)
+  end
+
+  # TODO: Make a refactor for dates validation
+  defp validate_end_date_greater_start_date(changeset, [start_date_field, end_date_field]) do
+    # TODO: use `with` here
+    start_date = get_field(changeset, start_date_field)
+    end_date = get_field(changeset, end_date_field)
+    do_compare_two_dates(changeset, :__shared__, end_date, start_date, "End date must be greater than start date")
+  end
+
+  def validate_current_or_future_date(changeset, field) do
+    case get_field(changeset, field) do
+      nil -> changeset
+      date -> do_compare_two_dates(changeset, field, date, DateTime.utc_now, "Date in the past")
+    end
+  end
+
+  defp do_compare_two_dates(changeset, _, nil, nil, _), do: changeset
+  defp do_compare_two_dates(changeset, field, start_date, end_date, message) do
+    case DateTime.compare(start_date, end_date) do
+      :lt -> add_error(changeset, field, message)
+      _ -> changeset
+    end
   end
 
   defp validate_required_at_least_one(changeset, [field | tail]) do
