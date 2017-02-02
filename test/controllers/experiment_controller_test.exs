027915f1ea883812369ffc92ceec658a9368474e
Dmitry Tymchuk <dsnipe@gmail.com>
2017-02-02 18:00:20 +0100

    Implement experiments create, update and index API endpoints

--- test/controllers/experiment_controller_test.exs
+++ test/controllers/experiment_controller_test.exs
@@ -8,8 +8,8 @@ defmodule Xperiments.ExperimentControllerTest do
       build_conn()
       |> put_req_header("accept", "application/json")
 
-    insert(:application, name: "frontend")
-    {:ok, conn: conn}
+    app = insert(:application, name: "frontend")
+    {:ok, conn: conn, app: app}
   end
 
   @api_path "/api/v1/applications/frontend"
@@ -28,5 +28,35 @@ defmodule Xperiments.ExperimentControllerTest do
     experiment = Repo.get_by!(Experiment, name: draft_experiment.name)
 
     assert body["experiment"]["id"] == experiment.id
+  end
+
+  test "/update any experiment with embded data", context do
+    exp_id = insert(:experiment).id
+    variant = %{
+      name: "Var A",
+      allocation: 40,
+      control_group: true,
+      description: "",
+      payload: "{}"
+    }
+    rule = %{
+      parameter: "language",
+      type: "string",
+      operator: "==",
+      value: "en"
+    }
+    updates = %{variants: [variant], rules: [rule]}
+    response = put(context[:conn], @api_path <> "/experiments/" <> exp_id, %{experiment: updates})
+    body = json_response(response, 200)
+
+    assert body["experiment"]["id"] == exp_id
+  end
+
+  test "/index returns list of experiments", context do
+    insert_list(3, :experiment, application: context.app)
+    body =
+      get(context[:conn], @api_path <> "/experiments")
+      |> json_response(200)
+    assert length(body["experiments"]) == 3
   end
 end
