ad6ef8d45c036b91e2705901541a4bf8da3dab48
Dmitry Tymchuk <dsnipe@gmail.com>
2017-02-07 14:27:29 +0100

    Tried to get pids of experiments from Registry, but it doesn't work

--- lib/xperiments/assigner/supervisor.ex
+++ lib/xperiments/assigner/supervisor.ex
@@ -18,16 +18,4 @@ defmodule Xperiments.Assigner.Supervisor do
     opts = [strategy: :one_for_one, name: Epxeriments.Supervisor]
     supervise(children, opts)
   end
-
-  def get_registry_experiments_pid do
-    Supervisor.which_children(Xperiments.Assigner.Supervisor)
-    |> Enum.filter(fn {id, _, _, _} -> id == Registry end)
-    |> List.first
-    |> elem(1) # get a Regisry pid from a tuple {id, pid, type, modules}
-  end
-
-  def get_registered_experiments do
-    Registry.keys(@registry_name, get_registry_experiments_pid())
-  end
-
 end
