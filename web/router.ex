e8018fd8e619cae8d05e60163e73269b81a419a7
Dmitry Tymchuk <dsnipe@gmail.com>
2017-03-02 11:34:01 +0100

    Fix an options route for an event call

--- web/router.ex
+++ web/router.ex
@@ -59,7 +59,7 @@ defmodule Xperiments.Router do
     options "/application/:app_name/experiments", AssignerController, :options
 
     post "/application/:app_name/experiments/events", AssignerController, :events
-    options "/applications/:app_name/experiments/events", AssignerController, :events
+    options "/application/:app_name/experiments/events", AssignerController, :events
 
     get "/application/:app_name/experiments/:id/variants/:variant_id", AssignerController, :example
     options "/application/:app_name/experiments/:id/variants/:variant_id", AssignerController, :example
