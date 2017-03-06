1d0a73c6c7cc51557dc2eefb6f66fbe03183f4f0
Dmitry Tymchuk <dsnipe@gmail.com>
2017-03-06 13:47:21 +0300

    Add type validations for rules
    Closes #25

--- web/models/rule.ex
+++ web/models/rule.ex
@@ -13,8 +13,8 @@ defmodule Xperiments.Rule do
   @primary_key false
   embedded_schema do
     field :parameter
-    field :type, :string, default: "string"
-    field :operator, :string, default: "=="
+    field :type, :string
+    field :operator, :string
     field :value
   end
 
@@ -24,8 +24,27 @@ defmodule Xperiments.Rule do
     struct
     |> cast(params, @allowed_params)
     |> validate_required(@allowed_params)
-    |> validate_inclusion(:type, ~w(string number))
+    |> validate_inclusion(:type, ~w(string number boolean))
     |> validate_inclusion(:operator, ~w(== != > < >= <=))
+    |> validate_string_type()
+    |> validate_boolean_type()
   end
 
+  def validate_string_type(chset) do
+    changes = chset.changes
+    if changes.type == "string" and not changes.operator in ["==", "!="] do
+      add_error(chset, :type, "For String types only '==' and '!=' operators are allowed")
+    else
+      chset
+    end
+  end
+
+  def validate_boolean_type(chset) do
+    changes = chset.changes
+    if changes.type == "boolean" and changes.operator != "==" and not changes.value in ["true", "false"]  do
+      add_error(chset, :type, "Boolean type must have only '==' operator and value should be 'true' or 'false'")
+    else
+      chset
+    end
+  end
 end
