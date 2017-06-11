1ffccdab08cd632280d4ffd4c9ae3be3125074b4
Dmitry Tymchuk <dsnipe@gmail.com>
2017-06-11 18:55:39 +0200

    Bump Phoenix to 1.3

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Xperiments.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0-rc.2", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:phoenix_html, "~> 2.6"},
