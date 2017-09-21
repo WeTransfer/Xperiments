20ae0bbffe8214e45e0ef2519b6ac46fa3a5aaca
Dmitry Tymchuk <dsnipe@gmail.com>
2017-09-21 16:09:06 +0200

    Add Recon library

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule Xperiments.Mixfile do
      {:remote_ip, "~> 0.1.0"},
      {:hammer, "~> 0.1.0"},
      {:hammer_backend_redis, "~> 0.1.0"},
+     {:recon, "~> 2.3.2"},
      {:ex_machina, "~> 1.0", only: :test},
      {:mock, "~> 0.2.0", only: :test}]
   end
