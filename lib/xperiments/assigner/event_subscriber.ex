e825829cd9d68803614350818e02b884af271aac
Dmitry Tymchuk <dsnipe@gmail.com>
2017-09-26 17:27:09 +0200

    Hibernate EventSubscriber as well
    Also copy a binary, when increasing an impreasion

--- lib/xperiments/assigner/event_subscriber.ex
+++ lib/xperiments/assigner/event_subscriber.ex
@@ -9,17 +9,17 @@ defmodule Xperiments.Assigner.EventSubscriber do
 
   def init(channel) do
     ref = Xperiments.Web.Endpoint.subscribe(channel)
-    {:ok, %{ref: ref}}
+    {:ok, %{ref: ref}, :hibernate}
   end
 
   def handle_info(%{event: "stop_experiment", payload: payload}, state) do
     Task.start(Experiment, :stop, [payload.id])
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   def handle_info(%{event: "run_experiment", payload: payload}, state) do
     Task.start(Experiment, :restart, [payload.id])
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   def handle_info(%{event: "terminate_experiment", payload: payload}, state) do
@@ -28,7 +28,7 @@ defmodule Xperiments.Assigner.EventSubscriber do
       ExperimentSupervisor.experiment_pids
       |> Enum.each(fn epid -> Experiment.remove_exclusion(epid, payload.id) end)
     end
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   def handle_info(%{event: "start_experiment", payload: %{experiment_data: experiment_data}}, state) do
@@ -36,16 +36,17 @@ defmodule Xperiments.Assigner.EventSubscriber do
       {:ok, _pid} = ExperimentSupervisor.start_experiment(experiment_data)
       Enum.each(experiment_data.exclusions, fn e -> Experiment.add_exclusion(e, experiment_data.id) end)
     end
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   def handle_info(%{event: "event:impression", payload: payload}, state) do
-    Experiment.inc_impression(payload.eid, payload.var_id)
-    {:noreply, state}
+    variant_id = :binary.copy(payload.var_id)
+    Experiment.inc_impression(payload.eid, variant_id)
+    {:noreply, state, :hibernate}
   end
 
   def handle_info(msg, state) do
     Logger.warn(inspect(msg))
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 end
